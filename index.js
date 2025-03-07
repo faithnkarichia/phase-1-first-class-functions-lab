08aaa73ed82b1c460eef504aa3ea16d0bc9ad5f6
Faith <kashfaith77@gmail.com>
2025-03-07 12:05:21 +0300

    Add functions

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(){
+   const divers=["Antonia","Nuru","Amari","Mo"]
+const newDivers= divers.slice(0,2)
+return newDivers
+}
+console.log(returnFirstTwoDrivers())
+
+function returnLastTwoDrivers(){
+    const divers=["Antonia","Nuru","Amari","Mo"] 
+    const lastTwoDiver= divers.slice(-2)
+    return lastTwoDiver
+}
+console.log(returnLastTwoDrivers())
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+console.log(selectingDrivers[0])
+console.log(selectingDrivers[1])
+
+
+function createFareMultiplier(num){
+    return function(value){
+return num*value
+    }
+
+}
+createFareMultiplier(5)
+
+function fareDoubler(fare){
+    return fare*2
+}
+fareDoubler()
+
+function fareTripler (fare){
+    return fare*3
+}
+fareTripler()
+
+// function selectDifferentDrivers(divers,returnFirstTwoDrivers,returnLastTwoDrivers){
+
+
+//     returnFirstTwoDrivers()
+//     returnLastTwoDrivers()
+// }
+function selectDifferentDrivers(driverArray, driverFunction) {
+    return driverFunction(driverArray);
+}
+selectDifferentDrivers()
